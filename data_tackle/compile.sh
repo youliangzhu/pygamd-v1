b9bc366b88c739158d99ea87c1be1a95e562c05c
youliangzhu <ylzhu@pygamd.com>
2022-10-11 21:33:48 +0800

    remove c++ codes

--- data_tackle/compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -c -std=c++11 MolInfo.cpp mst_reader.cpp Dcdbuilder.cpp Functions.cpp dataTackle.cpp -I /usr/local/cuda/include/ 
-nvcc -c -DNVCC rdf.cu strfac.cu 
-nvcc rdf.o strfac.o Functions.o MolInfo.o mst_reader.o Dcdbuilder.o dataTackle.o -o dataTackle
-rm *.o
