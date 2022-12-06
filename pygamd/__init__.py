5d240de0eb4316ff6ab40f71c27925e4aec11b06
youliangzhu <ylzhu@pygamd.com>
2022-12-06 15:35:30 +0800

    update library

--- pygamd/__init__.py
+++ pygamd/__init__.py
@@ -96,7 +96,7 @@ from optparse import OptionParser
 
 
 
-PYGAMD_VERSION="1.1.0"
+PYGAMD_VERSION="1.1.1"
 
 print("PYGAMD v", PYGAMD_VERSION )
 print("PYGAMD - Python GPU-Accelerated Molecular Dynamics Software")
