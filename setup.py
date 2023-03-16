ffe211df1b331f0eb53804f384706553634bf274
youliangzhu <ylzhu@pygamd.com>
2023-03-16 13:22:23 +0800

    support compute capability 3.5

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='pygamd',
-      version='1.2.4',
+      version='1.2.5',
       description='Python GPU-Accelerated Molecular Dynamics Software',
       long_description=long_description,
       classifiers=[
