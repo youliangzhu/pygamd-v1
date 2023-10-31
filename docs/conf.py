7f9f681cf603a74a58532c04b210e7209d9cd227
youliangzhu <ylzhu@pygamd.com>
2023-10-31 18:24:18 +0800

    update docs12

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@
 import os
 import sys
 import sphinx
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -32,7 +33,6 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
@@ -99,12 +99,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
-}
-
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
